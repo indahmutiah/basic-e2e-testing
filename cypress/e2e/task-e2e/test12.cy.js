b57c9c533b2c85dc92b6b1bf1d0005a6d1360df8
Indah Mutiah MZ <indahmutiahmz@gmail.com>
2025-05-17 22:17:00 +0700

    feat: task num 2

--- cypress/e2e/task-e2e/test12.cy.js
+++ cypress/e2e/task-e2e/test12.cy.js
@@ -1,4 +1,10 @@
-let baseUrl = "https://opensource-demo.orangehrmlive.com/web/";
+let baseUrl =
+  "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+let dropdownRole = `//label[text()='User Role']/following::div[contains(@class,'oxd-select-text-input')][1]`;
+let dropdownStatus = `//label[text()='Status']/following::div[contains(@class,'oxd-select-text-input')][1]`;
+let userSuggestion = `//input[@placeholder='Type for hints...']`;
+let userSuggestionResult = `//div[@role='listbox']//div[@role='option'][1]`;
 
 describe("Task E2E Test", () => {
   context("1. Menambahkan Karyawan Baru", () => {
@@ -28,13 +34,9 @@ describe("Task E2E Test", () => {
       cy.xpath(`(//input[@type='password'])[2]`).type("indah123");
 
       cy.xpath(`//button[normalize-space()='Save']`).click();
+      cy.wait(5000);
     });
     it("1.3 Buat Akun Untuk Karyawan", () => {
-      let dropdownRole = `//label[text()='User Role']/following::div[contains(@class,'oxd-select-text-input')][1]`;
-      let dropdownStatus = `//label[text()='Status']/following::div[contains(@class,'oxd-select-text-input')][1]`;
-      let userSuggestion = `//input[@placeholder='Type for hints...']`;
-      let userSuggestionResult = `//div[@role='listbox']//div[@role='option'][1]`;
-
       cy.xpath(`//span[text()='Admin']`).click();
       cy.xpath(`//button[contains(., 'Add')]`).click();
       cy.xpath(dropdownRole).click();
@@ -54,6 +56,39 @@ describe("Task E2E Test", () => {
       cy.xpath(`(//input[@type='password'])[2]`).type("indah123");
 
       cy.xpath(`//button[normalize-space()='Save']`).click();
+    });
+  });
+
+  context("2. Menambahkan Jatah Cuti Karyawan", () => {
+    beforeEach(() => {
+      cy.visit(baseUrl);
+
+      // Login
+      cy.xpath(`//input[@placeholder='Username']`).type("Admin");
+      cy.xpath(`//input[@placeholder='Password']`).type("admin123");
+      cy.xpath(`//button[normalize-space()='Login']`).click();
+      cy.url().should("include", "/dashboard");
+    });
+    // Menambahkan Jatah Cuti Karyawan
+    it("2.1 Menambahkan Jatah Cuti Karyawan", () => {
+      let dropdownLeave = `//div[@class="oxd-select-text-input" and text()='-- Select --']`;
+      cy.xpath(`//span[text()='Leave']`).click();
+      cy.wait(2000);
+      cy.get(
+        "li.oxd-topbar-body-nav-tab.--parent span.oxd-topbar-body-nav-tab-item"
+      )
+        .contains("Entitlements")
+        .click();
+      cy.xpath(`//ul[@class='oxd-dropdown-menu']/li[1]/a`).click();
+      cy.xpath(userSuggestion).type("Indah Mutiah");
+      cy.wait(2000);
+      cy.xpath(userSuggestionResult).click();
+      cy.xpath(dropdownLeave).click();
+      cy.xpath(`//div[@role='listbox']//div[@role='option'][5]`).click();
+      cy.xpath(`//label[text()='Entitlement']/following::input[1]`).type(12);
+      cy.xpath(`//button[normalize-space()='Save']`).click();
+
+      cy.xpath(`//button[normalize-space()='Confirm']`).click();
     });
   });
 });
